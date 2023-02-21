20e5e0eaf635659632e84fc4739afed23b8ce4fc
Osah Prince <4helpteam@gmail.com>
2023-02-21 23:44:05 +0100

    Time: 20 ms (12.90%), Space: 19.3 MB (32.26%) - LeetHub

--- 0258-add-digits/0258-add-digits.php
+++ 0258-add-digits/0258-add-digits.php
@@ -6,15 +6,21 @@ class Solution {
      */
     function addDigits($num) {
         // If num length is == 1 return $num
-        if(strlen($num) == 1 || strlen($num) == 0) return $num;
-        // Explode the number and add the values
-        $num = str_split($num);
-        $sum = 0;
-        for($i = 0; $i < count($num); $i++)
-        {
-            $sum += $num[$i];
-        }
+//         if(strlen($num) == 1 || strlen($num) == 0) return $num;
+//         // Explode the number and add the values
+//         $num = str_split($num);
+//         $sum = 0;
+//         for($i = 0; $i < count($num); $i++)
+//         {
+//             $sum += $num[$i];
+//         }
         
-        return $this->addDigits($sum);
+//         return $this->addDigits($sum);
+        
+        # ANOTHER SOLUTION
+        # ANOTHER SOLUTION
+        if(strlen($num) == 1 || strlen($num) == 0) return $num;
+        $num = array_sum(str_split($num));
+        return $this->addDigits($num);
     }
 }
