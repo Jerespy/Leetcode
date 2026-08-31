32c2e41fe72749068671f0597d9a49004e2cf6f5
Osah Prince <15744771+Jerespy@users.noreply.github.com>
2026-08-31 16:08:13 +0100

    Update latest Validate Binary Search Tree (java)

--- Medium/0098-validate-binary-search-tree/solution.java
+++ Medium/0098-validate-binary-search-tree/solution.java
@@ -2,8 +2,8 @@
  * Difficulty: Medium
  * Topics: Tree, Depth-First Search, Binary Search Tree, Binary Tree
  * Language: java
- * Runtime: 3 ms | Memory: 42.4 MB
- * Submitted: 2026-08-31T15:06:18.036Z
+ * Runtime: N/A | Memory: N/A
+ * Submitted: 2026-08-31T15:08:10.004Z
  */
 
 /**
@@ -23,19 +23,19 @@
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        return isValidBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE );
+        long low = Long.MIN_VALUE, high = Long.MAX_VALUE;
+        return isValidBST(root, low, high );
 
     }
 
     public boolean isValidBST(TreeNode root, long low, long high) {
-        System.out.println("High="+ high);
         if (root == null) return true;
         // if node isn't between low and high false;
         // go left = isValidBST(root.left,low, root.val); //Going left were we have lower values, decrease the high
         // go right = isValidBST(root.right,low, root.val); //Going right were we have higher values, increase the low
         // compare left && right
 
-        if (! (low <= root.val && high >= root.val)) return false;
+        if (! (low < root.val && high > root.val)) return false;
         boolean left = isValidBST(root.left, low, root.val);
         boolean right = isValidBST(root.right, root.val, high);
 
